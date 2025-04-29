693bb81545d1eddb4282c2d59e5a70837157c8b5
Murilo-Capristo <murilocapristo@gmail.com>
2025-04-29 20:58:29 -0300

    ARRUMEI

--- src/main/java/br/com/fiap/finance_walk_api/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/finance_walk_api/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         return http
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/transactions/**").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.POST, "/users/***").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
 
                 .anyRequest().authenticated()
             )
